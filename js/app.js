12ed827f886f2abdf06211d1a556ed9e600f0583
Scream8529 <scream8529@icloud.com>
2021-02-24 13:40:20 +0200

    update JS and new modal window

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ let quests = document.querySelectorAll("#quest")
 let img = document.querySelectorAll("#img")
 let text = document.querySelectorAll("#text")
 
-alert(quests)
+
+
 for (let i = 0; i < quests.length; i++) {
     quests[i].addEventListener('click', toggleFunction);
      function toggleFunction(){
@@ -12,5 +13,30 @@ for (let i = 0; i < quests.length; i++) {
     
 }
 
+
+let imgModal = document.querySelectorAll(".photos img");
+let modalImgOn = document.querySelector(".modalImgs")
+let modalImgsOn = document.querySelector(".modalImg")
+let image = document.querySelector(".modalImg img")
+let bodyOverflow = document.querySelector("body");
+
+modalImgOn.addEventListener("click", modalImgOff)
+function modalImgOff (){
+   modalImgOn.classList.remove("on")
+   bodyOverflow.style.overflow = "auto"
+   modalImgsOn.classList.remove("on")
+}
+for (let b = 0; b < imgModal.length; b++){
+   imgModal[b].addEventListener("click", imgModalOn);
+   function imgModalOn(){
+      modalImgsOn.classList.add("on")
+      modalImgOn.classList.add("on");
+      bodyOverflow.style.overflow = "hidden"
+      let newImg = imgModal[b].getAttribute("src")
+      image.setAttribute("src", newImg)
+      
+   }
+
+}
 
 
