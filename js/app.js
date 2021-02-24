3100d0a58125db8c76f8fbb6166de3c953374fae
Pavel <78850677+Scream8529@users.noreply.github.com>
2021-02-24 11:45:53 +0200

    Remake jsCode

--- js/app.js
+++ js/app.js
@@ -1,33 +1,16 @@
-let question1 = document.querySelector('#ques1');
-let question2 = document.querySelector('#ques2');
-let question3 = document.querySelector('#ques3');
-let question4 = document.querySelector('#ques4');
-let text1 = document.querySelector('#text1');
-let text2 = document.querySelector('#text2');
-let text3 = document.querySelector('#text3');
-let text4 = document.querySelector('#text4');
-let img1 = document.querySelector('#img1')
-let img2 = document.querySelector('#img2')
-let img3 = document.querySelector('#img3')
-let img4 = document.querySelector('#img4')
+let quests = document.querySelectorAll("#quest")
+let img = document.querySelectorAll("#img")
+let text = document.querySelectorAll("#text")
 
-let quest1= function(){
-    text1.classList.toggle("on")
-    img1.classList.toggle("on")
+alert(quests)
+for (let i = 0; i < quests.length; i++) {
+    quests[i].addEventListener('click', toggleFunction);
+     function toggleFunction(){
+        img[i].classList.toggle("on");
+        text[i].classList.toggle("on");
+     } 
+    
 }
-let quest2= function(){
-    text2.classList.toggle("on")
-    img2.classList.toggle("on")
-}
-let quest3= function(){
-    text3.classList.toggle("on")
-    img3.classList.toggle("on")
-}
-let quest4= function(){
-    text4.classList.toggle("on")
-    img4.classList.toggle("on")
-}
-
 
 
 
